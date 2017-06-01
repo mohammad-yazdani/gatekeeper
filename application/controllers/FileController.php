4b13e6e150fd21e8b04a90beea767e39ae384430
Kuokhin Ieong <kieong@OGAM.local>
2017-06-01 10:57:58 -0400

    FileController class updated

--- application/controllers/FileController.php
+++ application/controllers/FileController.php
@@ -7,6 +7,8 @@
  */
 
 namespace controllers;
+namespace models;
+require_once 'File.php';
 
 
 use DAO\ClientFileDAOImpl;
@@ -30,41 +32,66 @@ class FileController extends \Controller
     public function get($key = NULL, $xss_clean = NULL)
     {
         // TODO: Implement get() method.
+        $id = (int) $key;
+        return $this->fileDAO->get($id);
     }
 
     public function post($key = NULL, $xss_clean = NULL)
     {
         // TODO: Implement post() method.
+        $json = json_decode($key);
+        $file = new File($json->clientId);
+        if($this->fileDAO->save($file)){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public function put($key = NULL, $xss_clean = NULL)
     {
         // TODO: Implement put() method.
+        $this->post($key); //temporary
     }
 
     public function delete($key = NULL, $xss_clean = NULL)
     {
         // TODO: Implement delete() method.
+        $json = json_decode($key);
+        $file = $this->fileDAO->get($json->id);
+        if ($file) {
+            if($this->fileDAO->delete($file)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 
     public function REST_GET($id, $token = NULL)
     {
         // TODO: Implement REST_GET() method.
+        $this->get($id);
     }
 
     public function REST_POST(string $json)
     {
         // TODO: Implement REST_POST() method.
+        $this->post($json);
     }
 
     public function REST_PUT(string $json)
     {
         // TODO: Implement REST_PUT() method.
+        $this->put($json);
     }
 
     public function REST_DELETE(string $json)
     {
         // TODO: Implement REST_DELETE() method.
+        $this->delete($json);
     }
 
 }
