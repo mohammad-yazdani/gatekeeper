6145f4911f0f8fc4df0bf5cc1cbd229587ed7d34
Mohammad Yazdani <myazdani@OGAM.local>
2017-07-20 17:27:46 -0400

    first commit

--- application/analytics/Functions/Macros/Update.vb
+++ application/analytics/Functions/Macros/Update.vb
@@ -11,14 +11,17 @@ Sub Update()
     Dim monthend_value As Date
 
     Set monthend_range = ThisWorkbook.ActiveSheet.Range("A1:H50").Find("Month End")
-    monthend_row = monthend_range.Row
-    monthend_col = monthend_range.Column + 3
+    monthend_row = monthend_range.row
+    monthend_col = monthend_range.Column + 1
+    Do While ThisWorkbook.ActiveSheet.Cells(monthend_row, monthend_col).Value = “”
+        monthend_col = monthend_col + 1
+    Loop
     monthend_value = DateSerial(Year(Date), Month(Date), 0)
     ThisWorkbook.ActiveSheet.Cells(monthend_row, monthend_col).Value = Format(monthend_value, "dd-mmm-yyyy")
 
 
     Set total_range = ThisWorkbook.ActiveSheet.Range("A1:D500").Find("Total")
-    total_row = total_range.Row
+    total_row = total_range.row
     total_col = total_range.Column
     prev_row = total_row - 1
 
@@ -26,6 +29,5 @@ Sub Update()
     old_tbill = ThisWorkbook.ActiveSheet.Cells(prev_row, total_col + 11).Value
     ThisWorkbook.ActiveSheet.Cells(prev_row, total_col).Value = Format(old_date, "dd-mmm-yyyy") ' Changes Date format
     ThisWorkbook.ActiveSheet.Cells(prev_row, total_col + 11).Value = Format(old_tbill, "0.0000%") ' Changes T-bill Rate format
-
 End Sub
 
